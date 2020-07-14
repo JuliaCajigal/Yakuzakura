dcb7eaadf914fbe8a54f65e9373cf48b133e5339
RaulArenas <43203292+RaulArenas@users.noreply.github.com>
2020-07-14 13:24:36 +0200

    Field of View (Origins)

---
+++ Yakuzakura/Assets/Scripts/IA/RotateFollowMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateFollowMouse : MonoBehaviour
+{
+    Camera viewCamera;
+    float angle;
+    Vector2 mousePos;
+
+    void Start()
+    {
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = viewCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), /* SPEED */10000 * Time.deltaTime);
+    }
+}
